3545aba4a5f68b2eecebdcde2e5b67bf6c9e3e3e
Victor Nogueira <felladrin@gmail.com>
2016-10-22 02:26:02 -0300

    Adjust app configurations

--- config/web.php
+++ config/web.php
@@ -3,13 +3,13 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'vanhackathon',
+    'name' => 'Couse Builder',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '14jzE4U2rpF3ZDra7Xr464SMNa9g0DKh',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -26,7 +26,7 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => YII_DEBUG,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -38,14 +38,10 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
         ],
-        */
     ],
     'params' => $params,
 ];
