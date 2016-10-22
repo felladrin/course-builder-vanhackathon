ad18e2b5cd0d3103172c24087c7017abbf92531b
Victor Nogueira <felladrin@gmail.com>
2016-10-22 14:10:48 -0300

    Allow user of Gii and Debugger by computers on LAN

--- config/web.php
+++ config/web.php
@@ -51,11 +51,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['192.168.*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['192.168.*'],
     ];
 }
 
